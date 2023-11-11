3e6e544f11c1522c5ea8d3495c8319514d23eb83
OjaanIr <farnasrozaan7@gmail.com>
2023-11-11 17:07:25 +0700

    feat: medical question classifier

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from med_question_classifier import MedQuestionClassifier
 from sentiment import SentimentClassifier
+
 def main():
+  med_question_clf = MedQuestionClassifier("./medical-bert-classifier")
   sentiment_clf = SentimentClassifier("./standard-bert-sentiment")
   
   while(True):
     text = input("Enter a sentence: ")
-    print(sentiment_clf.predict(text))
+    res = med_question_clf.predict(text)
+    print("This sentence is", res)
+    if res == "medical":
+      print(sentiment_clf.predict(text))
+    else:
+      return
     print("")
-
 
 if __name__ == "__main__":
   main()
---
+++ med_question_classifier.py
@@ -0,0 +1,26 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+
+
+class MedQuestionClassifier(object):
+  def __init__(self, path, device = None) -> None:
+    self.path = path
+    self.device = device if device != None else torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    self.load_model()
+    self.label_mapper = {
+      0: "non-medical",
+      1: "medical"
+    }
+
+  def load_model(self):
+    self.model = AutoModelForSequenceClassification.from_pretrained(self.path)
+    self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    self.model.to(self.device)
+
+  def predict(self, text):
+    with torch.no_grad():
+      inputs = self.tokenizer(text, return_tensors="pt")
+      inputs.to(self.device)
+      outputs = self.model(**inputs)
+      logits = outputs[0]
+      return self.label_mapper[logits.argmax(-1).item()]
