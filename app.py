7c6c457841454f8f0afa1b36f6363f68e98cdb33
Averrous Saloom <averous009@gmail.com>
2023-11-12 16:32:30 +0700

    feat: add similarity and classification

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from sentiment import SentimentClassifier
+from similarity import SimilarityClassifier
+from med_question_classifier import MedQuestionClassifier
 
 app = Flask(__name__,static_folder='static')
 CORS(app, origins=["*"])
 MODELS = {
   "sentiment": SentimentClassifier("./standard-bert-sentiment"),
+  "similarity": SimilarityClassifier("./question-similarity-bert"),
+  "classification": MedQuestionClassifier("./medical-bert-classifier")
 }
 
 @app.route("/")
@@ -16,15 +20,26 @@ def main():
 def predict(task):
   if task not in MODELS:
     return "Task not found", 404
-  text = request.args.get("text", default="", type=str)
-  if text == "":
-    return "Fill in the text query to predict", 404
+  
   model = MODELS[task]
+
+  if task == "similarity":
+    text1 = request.args.get("text1", default="", type=str)
+    text2 = request.args.get("text2", default="", type=str)
+
+    if text1 == "" or text2 == "":
+      return "Fill in the text1 and text2 query to predict", 404
+    pred = model.predict(text1, text2)
+  else:
+    text = request.args.get("text", default="", type=str)
+    if text == "":
+      return "Fill in the text query to predict", 404
+    pred = model.predict(text)
 
   response = {
     "task": task,
-    "text": text,
-    "prediction": model.predict(text)
+    "text": text if task != "similarity" else [text1, text2],
+    "prediction": pred
   }
 
   return jsonify(response)
--- static/script/index.js
+++ static/script/index.js
@@ -1,9 +1,11 @@
 
 API_URL = "http://localhost:5000"
+RED = "bg-red-500"
+GREEN = "bg-green-500"
 
-function predict_sentiment(text){
+function predict_sentiment(text, target){
   if (text === ""){
-    document.getElementById("sentiment").innerText = "empty"
+    target.innerText = "empty"
     return
   }
   fetch(API_URL + `/nlp/sentiment/predict?text=${text}`, {
@@ -14,12 +16,64 @@ function predict_sentiment(text){
     if (data !== undefined){
        result = data["prediction"]
     }
-    document.getElementById("sentiment").innerText = result
+    target.innerText = result
   })
 }
 
-document.getElementById("text").addEventListener("input", (e) => {
+function predict_classification(text, target){
+  if (text === ""){
+    target.innerText = "unknown"
+    return
+  }
+  fetch(API_URL + `/nlp/classification/predict?text=${text}`, {
+    method: "GET"
+  }).then(response => response.json())
+  .then(data => {
+    let result = "breaking the api"
+    if (data !== undefined){
+       result = data["prediction"]
+    }
+    target.innerText = result
+  })
+}
+
+function predict_similarity(text1, text2, target){
+  if (text1 === "" || text2 === ""){
+    target.innerText = "unknown"
+    return
+  }
+  fetch(API_URL + `/nlp/similarity/predict?text1=${text1}&text2=${text2}`, {
+    method: "GET"
+  }).then(response => response.json())
+  .then(data => {
+    let result = "breaking the api"
+    if (data !== undefined){
+       result = data["prediction"]
+    }
+    target.innerText = result
+    if (result === "unsimilar"){
+      target.classList.remove(GREEN)
+      target.classList.add(RED)
+    } else {
+      target.classList.remove(RED)
+      target.classList.add(GREEN)
+    }
+  })
+}
+
+document.getElementById("text1").addEventListener("input", (e) => {
   e.preventDefault()
   const text = e.target.value
-  predict_sentiment(text)
+  predict_sentiment(text, document.getElementById("sentiment1"))
+  predict_classification(text, document.getElementById("classification1"))
+  predict_similarity(text, document.getElementById("text2").value, document.getElementById("similarity"))
 })
+
+document.getElementById("text2").addEventListener("input", (e) => {
+  e.preventDefault()
+  const text = e.target.value
+  predict_sentiment(text, document.getElementById("sentiment2"))
+  predict_classification(text, document.getElementById("classification2"))
+  predict_similarity(document.getElementById("text1").value, text, document.getElementById("similarity"))
+})
+
